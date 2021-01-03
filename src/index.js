a1b6cfc05c4848bbb6e7ce854aae6bcdaf8d697e
fidangithub <57586245+fidangithub@users.noreply.github.com>
2021-01-03 19:40:45 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ sagaMiddleware.run(watchDiscover);
 
 const app = (
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <App />
         </BrowserRouter>
     </Provider>
